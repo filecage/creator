159f846a487f90445076831aedcf3ebe1fc0aada
David Beuchert <mail@dbeuchert.com>
2015-12-23 08:49:14 +0100

    implements correct interface of Unresolvable exception

--- lib/Exceptions/Unresolvable.php
+++ lib/Exceptions/Unresolvable.php
@@ -4,4 +4,32 @@
 
     class Unresolvable extends \Exception {
 
+        /**
+         * @var string
+         */
+        private $message;
+
+        /**
+         * @var string
+         */
+        private $class;
+
+        /**
+         * @param string $message
+         * @param string $class
+         */
+        function __construct ($message, $class = null) {
+            $this->message = $message;
+            $this->class = $class;
+
+            parent::__construct($this->getFullMessage());
+        }
+
+        /**
+         * @return string
+         */
+        function getFullMessage () {
+            return ($this->class) ? sprintf('%s when creating %s', $this->message, $this->class) : $this->message;
+        }
+
     }
