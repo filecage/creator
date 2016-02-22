e1cbbff7941648549b92a000d09383bbea4d84ce
David Beuchert <mail@dbeuchert.com>
2016-02-22 04:08:50 +0100

    moved creation logic from Creator class to Creation and ResourceRegistry

--- lib/Creator.php
+++ lib/Creator.php
@@ -3,23 +3,20 @@
     namespace Creator;
 
     use Creator\Exceptions\Unresolvable;
-    use Creator\Interfaces\Factory;
-    use ReflectionException;
-    use ReflectionClass;
-    use ReflectionMethod;
-    use ReflectionParameter;
 
     class Creator {
 
         /**
-         * @var array
+         * @var ResourceRegistry
          */
-        private $classResourceRegistry = [];
+        private $resourceRegistry;
 
         /**
-         * @var array
+         * @param ResourceRegistry $resourceRegistry
          */
-        private $primitiveResourceRegistry = [];
+        function __construct(ResourceRegistry $resourceRegistry = null) {
+            $this->resourceRegistry = ($resourceRegistry) ?: new ResourceRegistry();
+        }
 
         /**
          * @param string $className
@@ -30,19 +27,7 @@
          * @return object
          */
         function create ($className, $bypassClassResourceRegistry = false) {
-            $object = $this->resolveClassAgainstRegistry($className);
-
-            if (!$object || $bypassClassResourceRegistry) {
-                $object = $this->createInstance(new ReflectionClass($className));
-
-                if ($bypassClassResourceRegistry) {
-                    return $object;
-                } else {
-                    $this->classResourceRegistry[$className] = $object;
-                }
-            }
-
-            return $object;
+            return (new Creation($className, ($bypassClassResourceRegistry) ? new ResourceRegistry() : $this->resourceRegistry))->create();
         }
 
         /**
@@ -52,8 +37,7 @@
          * @return $this
          */
         function registerClassResource ($instance, $classResourceKey = null) {
-            $classResourceKey = $classResourceKey ?: get_class($instance);
-            $this->classResourceRegistry[$classResourceKey] = $instance;
+            $this->resourceRegistry->registerClassResource($instance, $classResourceKey);
 
             return $this;
         }
@@ -65,192 +49,9 @@
          * @return $this
          */
         function registerPrimitiveResource ($resourceKey, $value) {
-            $this->primitiveResourceRegistry[$resourceKey] = $value;
+            $this->resourceRegistry->registerPrimitiveResource($resourceKey, $value);
 
             return $this;
-        }
-
-        /**
-         * @param ReflectionClass $reflector
-         *
-         * @return object
-         * @throws Unresolvable
-         */
-        private function createInstance(ReflectionClass $reflector) {
-            return ($reflector->isInstantiable()) ? $this->createInstanceFromReflectionClass($reflector) : $this->createInstanceFromUninstantiableReflectionClass($reflector);
-        }
-
-        /**
-         * @param string $className
-         *
-         * @return object
-         */
-        private function resolveClassAgainstRegistry ($className) {
-            if (isset($this->classResourceRegistry[$className])) {
-                return $this->classResourceRegistry[$className];
-            }
-
-            return null;
-        }
-
-        /**
-         * @param ReflectionClass $reflector
-         *
-         * @return object
-         * @throws Unresolvable
-         */
-        private function createInstanceFromUninstantiableReflectionClass (ReflectionClass $reflector) {
-            if ($reflector->implementsInterface(Interfaces\Singleton::class)) {
-                return $this->getInstanceFromSingleton($reflector);
-            } elseif ($reflector->isInterface() || $reflector->isAbstract()) {
-                $factoryReflector = $this->getFactoryClassReflector($reflector);
-
-                return $this->createInstanceFromFactoryReflector($reflector, $factoryReflector);
-            } else {
-                throw new Unresolvable('Class is neither instantiable nor implements Singleton interface', $reflector->getName());
-            }
-        }
-
-        /**
-         * @param ReflectionClass $reflector
-         *
-         * @throws Unresolvable
-         * @return object
-         */
-        private function createInstanceFromReflectionClass (ReflectionClass $reflector) {
-            $constructor = $reflector->getConstructor();
-            if (!$constructor) {
-                return $reflector->newInstance();
-            }
-
-            try {
-                return $reflector->newInstanceArgs($this->collectAndResolveMethodDependencies($constructor));
-            } catch (ReflectionException $e) {
-                throw new Unresolvable('Dependencies can not be resolved: ' . $e->getMessage(), $reflector->getName());
-            }
-        }
-
-        /**
-         * @param ReflectionClass $reflector
-         * @param ReflectionClass $factoryReflector
-         *
-         * @return $this
-         * @throws Unresolvable
-         */
-        private function createInstanceFromFactoryReflector (ReflectionClass $reflector, ReflectionClass $factoryReflector) {
-            $factory = $this->createInstance($factoryReflector);
-            if (!$factory instanceof Factory) {
-                throw new Unresolvable('Factory ' . $factoryReflector->getName() . ' does not implement required interface Creator\\Interfaces\\Factory', $reflector->getName());
-            }
-
-            $class = $factory->createInstance();
-            if (!$reflector->isInstance($class)) {
-                throw new Unresolvable('Create method of factory ' . $factoryReflector->getName() . ' did not return instance of ' . $reflector->getName() . ' class', $reflector->getName());
-            }
-
-            return $class;
-        }
-
-        /**
-         * @param ReflectionClass $reflector
-         *
-         * @return object
-         */
-        private function getInstanceFromSingleton (ReflectionClass $reflector) {
-            return $reflector->getMethod('getInstance')->invoke(null);
-        }
-
-        /**
-         * @param ReflectionMethod $reflectionMethod
-         *
-         * @return array
-         * @throws \Exception
-         */
-        private function collectAndResolveMethodDependencies (ReflectionMethod $reflectionMethod) {
-            $dependencies = [];
-            foreach ($reflectionMethod->getParameters() as $parameter) {
-                $dependencies[] = $this->resolveParameterDependency($parameter);
-            }
-
-            return $dependencies;
-        }
-
-        /**
-         * @param ReflectionParameter $parameter
-         *
-         * @return mixed|object
-         * @throws Unresolvable
-         * @throws ReflectionException
-         */
-        private function resolveParameterDependency (ReflectionParameter $parameter) {
-            $class = $parameter->getClass();
-            if ($class) {
-                return $this->getClassResource($class->getName());
-            }
-
-            try {
-                $primitiveResource = $this->getPrimitiveResource($parameter->getName());
-
-                return $primitiveResource;
-            } catch (Unresolvable $e) {
-                if ($parameter->isDefaultValueAvailable()) {
-                    return $parameter->getDefaultValue();
-                }
-                throw $e;
-            }
-        }
-
-        /**
-         * @param string $classResourceKey
-         *
-         * @return object
-         */
-        private function getClassResource ($classResourceKey) {
-            if (!isset($this->classResourceRegistry[$classResourceKey])) {
-                $this->classResourceRegistry[$classResourceKey] = $this->create($classResourceKey);
-            }
-
-            return $this->classResourceRegistry[$classResourceKey];
-        }
-
-        /**
-         * @param string $resourceKey
-         *
-         * @return mixed
-         * @throws Unresolvable
-         */
-        private function getPrimitiveResource ($resourceKey) {
-            if (!isset($this->primitiveResourceRegistry[$resourceKey])) {
-                throw new Unresolvable('Tried to load dependency "' . $resourceKey . '" with unknown primitive resource');
-            }
-
-            return $this->primitiveResourceRegistry[$resourceKey];
-        }
-
-        /**
-         * @param string $className
-         *
-         * @return string
-         */
-        private function buildFactoryClassName ($className) {
-            return sprintf('%sFactory', $className);
-        }
-
-        /**
-         * @param ReflectionClass $reflector
-         *
-         * @return ReflectionClass
-         * @throws Unresolvable
-         */
-        private function getFactoryClassReflector (ReflectionClass $reflector) {
-            $factoryClassName = $this->buildFactoryClassName($reflector->getName());
-            try {
-                $factoryClass = new ReflectionClass($factoryClassName);
-            } catch (ReflectionException $e) {
-                throw new Unresolvable('Can not load factory class "' . $factoryClassName . '": ' . $e->getMessage(), $reflector->getName());
-            }
-
-            return $factoryClass;
         }
 
     }
