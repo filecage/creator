daad1577249eea27f69186be77957ca518a71a96
David Beuchert <mail@dbeuchert.com>
2018-06-29 14:29:31 +0200

    adds tests for factory / creation caches

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -161,6 +161,41 @@
             $this->assertSame($injectedSimpleClass, $extendedClass->getSimpleClass());
         }
 
+        function testExpectsFactoryResultToBeCached () {
+            $creator = new Creator();
+
+            $simpleClass = new SimpleClass();
+            $creator->registerFactory( function() use ($simpleClass) {
+                return new ExtendedClass($simpleClass);
+            }, ExtendedClass::class);
+
+            $firstExtendedClass = $creator->create(ExtendedClass::class);
+            $secondExtendedClass = $creator->create(ExtendedClass::class);
+
+            $this->assertInstanceOf(ExtendedClass::class, $firstExtendedClass);
+            $this->assertSame($firstExtendedClass, $secondExtendedClass);
+        }
+
+        function testExpectsInjectedFactoryResultNotToBeCached () {
+            $creator = new Creator();
+            $simpleClass = new SimpleClass();
+
+            /** @var ExtendedClass $factoryExtendedClass */
+            $factoryExtendedClass = $creator->createInjected(ExtendedClass::class)
+                ->withFactory(function() use ($simpleClass) {
+                    return new ExtendedClass($simpleClass);
+                }, ExtendedClass::class)
+                ->create();
+
+            /** @var ExtendedClass $defaultExtendedClass */
+            $defaultExtendedClass = $creator->create(ExtendedClass::class);
+
+            $this->assertInstanceOf(ExtendedClass::class, $factoryExtendedClass);
+            $this->assertNotSame($factoryExtendedClass, $defaultExtendedClass);
+            $this->assertSame($simpleClass, $factoryExtendedClass->getSimpleClass());
+            $this->assertNotSame($simpleClass, $defaultExtendedClass->getSimpleClass());
+        }
+
         function testExpectsInvalidFactoryExceptionInGlobalRegistry () {
             $this->expectException(InvalidFactoryException::class);
             $this->expectExceptionMessageRegExp('/Trying to register unsupported factory type ".+" for class ".+"/');
