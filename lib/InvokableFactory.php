ea16bc4d692f21ca99350646e73a5ee0938dd4a8
David Beuchert <mail@dbeuchert.com>
2018-07-02 17:49:27 +0200

    InvokableFactory now supports class name bound factories

--- lib/InvokableFactory.php
+++ lib/InvokableFactory.php
@@ -5,18 +5,68 @@
     use Creator\Exceptions\InvalidFactory;
     use Creator\Interfaces\Factory;
 
-    abstract class InvokableFactory extends Invokable {
+    class InvokableFactory extends Invokable {
 
+        /**
+         * @var Creatable
+         */
+        private $factoryCreatable;
+
+        /**
+         * @param $factory
+         *
+         * @return Invokable|InvokableFactory
+         * @throws InvalidFactory
+         */
         static function createFromAnyFactory ($factory) {
             if (is_callable($factory)) {
                 $invokable = InvokableClosure::createFromCallable($factory);
             } elseif ($factory instanceof Factory) {
                 $invokable = InvokableClosure::createFromCallable([$factory, 'createInstance']);
+            } elseif (is_string($factory) && class_exists($factory)) {
+                $factoryCreatable = new Creatable($factory);
+                if (!$factoryCreatable->getReflectionClass()->implementsInterface(Factory::class)) {
+                    throw new InvalidFactory(get_class($factory));
+                }
+
+                $invokable = new InvokableFactory($factoryCreatable);
+
             } else {
                 throw InvalidFactory::createWithUnknownActualType($factory);
             }
 
             return $invokable;
+        }
+
+        /**
+         * @param Creatable $factoryCreatable
+         */
+        function __construct (Creatable $factoryCreatable) {
+            parent::__construct($factoryCreatable->getInvokableReflection());
+            $this->factoryCreatable = $factoryCreatable;
+        }
+
+        /**
+         * @return DependencyContainer
+         */
+        function getDependencies () {
+            return (new DependencyContainer())
+                ->addDependency(Dependency::createFromCreatable('factory', $this->factoryCreatable));
+        }
+
+        /**
+         * @param array $args
+         *
+         * @return mixed
+         * @throws InvalidFactory
+         */
+        function invoke (array $args = []) {
+            $factory = $args[0];
+            if (!$factory instanceof Factory) {
+                throw new InvalidFactory(get_class($factory), 'lazy bound Factory');
+            }
+
+            return $factory->createInstance();
         }
 
     }
