4c5d206b94d25b333f1b555830304f1a61314aa4
filecage <mail@dbeuchert.com>
2019-05-20 12:18:54 +0200

    load test namespaces via autoload-dev

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,3 @@
 <?php
 
-    /** @var \Composer\Autoload\ClassLoader $autoloader */
-    $autoloader = require dirname(__DIR__) . '/vendor/autoload.php';
-    $autoloader->addPsr4('Creator\Tests\\', __DIR__);
+    require dirname(__DIR__) . '/vendor/autoload.php';
